5940615ba9535ddcc63e6574226488ca4ed32588
lnc4288 <lnc4288@outlook.com>
2017-07-10 12:43:11 -0600

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const post = require('../models/post');
+
+const db = "mongodb://dache2:codeposts101!@ds131621.mlab.com:31621/codepostnet";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(db, function(err) {
+    if(err) {
+        console.log('Connection error');
+    }
+});
+
+router.get('/posts', function(req, res) {
+    console.log('Requesting posts');
+    post.find({})
+        .exec(function(err, posts) {
+            if (err) {
+                console.log('Error getting the posts');
+            } else {
+                res.json(posts);
+            }
+        });
+});
+
+router.get('/details/:id', function(req, res) {
+    console.log('Requesting post');
+    post.findById(req.params.id)
+        .exec(function(err, post) {
+            if (err) {
+                console.log('Error getting the post');
+            } else {
+                res.json(post);
+            }
+        });
+});
+
+router.post('/posts', function(req, res) {
+    console.log('Posting a post');
+    var newPost = new post();
+    newPost.title = req.body.title;
+    newPost.url = req.body.url;
+    newPost.description = req.body.description;
+    newPost.save(function(err, addedPost) {
+        if (err) {
+            console.log('Error inserting the post');
+        } else {
+            res.json(addedPost);
+        }
+    });
+});
+
+
+module.exports = router;
